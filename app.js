6c2afa237682e515182ea3542b960d753daa9634
splaza28 <global-splaza28@gmail.com>
2022-01-25 23:47:08 -0600

    added css stykesheet

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 app.listen(PORT, () => {
