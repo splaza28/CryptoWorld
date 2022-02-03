4bdbcacca38644e9b782dad548d7a7baa69f6f62
splaza28 <=>
2022-02-03 15:34:19 -0600

    fixed typo in register hbs

--- models/comment.js
+++ models/comment.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = require('../config/database');
+const db = require('../config/connection');
 
 const Comment = db.define('Comment', {
     comment: {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = require('../config/database');
+const db = require('../config/connection');
 
 const Userdata = db.define('Userdata', {
   fullname: {
