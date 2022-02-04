c73e07ac0985b1cbff8e1cafded037d165a66070
splaza28 <=>
2022-02-04 00:37:38 -0600

    server update

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const env = require('dotenv');
 const path = require('path');
-var cookieParser = require('cookie-parser');
 const sequelize = require("./config/connection");
+const PORT = process.env.PORT || 3002;
+var cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -13,20 +14,14 @@ env.config();
 
 const static_path = path.join(__dirname, "./public");
 app.use(express.static(static_path));
-
-// HBS
 app.set("view engine", "hbs");
-
-// Express body parser
 app.use(express.urlencoded({ extended: false }));
-
-// initialize cookie-parser to allow us access the cookies stored in the browser. 
 app.use(cookieParser());
+app.use(flash());
 
-// initialize express-session to allow us track the logged-in user across sessions.
 app.use(session({
   key: 'user_sid',
-  secret: 'some randon things',
+  secret: 'something random',
   resave: false,
   saveUninitialized: false,
   cookie: {
@@ -42,10 +37,8 @@ app.use((req, res, next) => {
 });
 
 
-// Connect flash
-app.use(flash());
 
-// Global variables
+
 app.use(function (req, res, next) {
   res.locals.success_msg = req.flash("success_msg");
   res.locals.error_msg = req.flash("error_msg");
@@ -53,11 +46,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-// Routes
+
 app.use(require("./routes/index.js"));
 app.use(require("./routes/users.js"));
 
-const PORT = process.env.PORT || 3002;
+
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log(`Now Listening ${PORT}!`));
